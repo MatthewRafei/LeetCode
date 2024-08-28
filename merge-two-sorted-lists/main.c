4d4ca7e5784f759f518d4f2716c7d60c21cbfc20
MatthewRafei <matthewrafei1@gmail.com>
2024-08-27 19:48:50 -0500

    Working on merge-two-sorted-lists

--- merge-two-sorted-lists/main.c
+++ merge-two-sorted-lists/main.c
@@ -14,24 +14,29 @@ struct ListNode {
 };
 
 struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2) {
-  struct ListNode arr[100];
+  struct ListNode arr[6];
   struct ListNode *n = list1;
   int i = 0;
-  
+
+  // Get to the end of the first list
   while(n->next != NULL) {
     n = n->next;
   }
 
+  // Combine lists
   n->next = list2;
   n = list1;
+  i = 0;
 
-  while(n->next != NULL) {
-    printf("The value of node %d is %d\n", i++, n->val);
+  // Put list in array
+  while(n != NULL) {
+    arr[i++] = *n;
     n = n->next;
   }
 
-
-  
+  for(;i >= 0; i--) {
+    printf("What is the value in the array at index %d : %d\n", i, arr[i]);
+  }
 }
 
 int main(void) {
@@ -46,6 +51,7 @@ int main(void) {
 
   list1.next = &list1_1;
   list1_1.next = &list1_2;
+  list1_2.next = NULL;
 
   struct ListNode list2;
   struct ListNode list2_1;
@@ -57,6 +63,7 @@ int main(void) {
 
   list2.next = &list2_1;
   list2_1.next = &list2_2;
+  list2_2.next = NULL;
   
 
   mergeTwoLists(&list1, &list2);
